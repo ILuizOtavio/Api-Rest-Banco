1dcd0fa23a72b8a209e628139f272194ecb3c393
luiz otavio <luizinho1710@gmail.com>
2023-10-11 15:26:55 -0300

    segundo commit

--- src/controladores/dados.js
+++ src/controladores/dados.js
@@ -65,9 +65,79 @@ const listarContas = (req, res) => {
   return res.json(contas);
 };
 
-const atualizarConta = (req, res) => {};
+const atualizarConta = (req, res) => {
+  const { numero } = req.params;
+  const { nome, cpf, data_nascimento, telefone, email, senha } = req.body;
 
-const deletarConta = (req, res) => {};
+  const conta = contas.find((conta) => {
+    return conta.numero === Number(numero);
+  });
+  if (!conta) {
+    return res.status(404).json({ mensagem: "conta não encontrada" });
+  }
+  if (!nome) {
+    return res.status(400).json({
+      mensagem: "O nome é obrigatório",
+    });
+  }
+
+  if (!cpf) {
+    return res.status(400).json({
+      mensagem: "O CPF é obrigatório",
+    });
+  }
+
+  if (!email) {
+    return res.status(400).json({
+      mensagem: "O E-mail é obrigatório",
+    });
+  }
+  if (!telefone) {
+    return res.status(400).json({
+      mensagem: "O telefone é obrigatório",
+    });
+  }
+  if (!data_nascimento) {
+    return res.status(400).json({
+      mensagem: "A data de nascimento é obrigatória",
+    });
+  }
+  if (!senha) {
+    return res.status(400).json({
+      mensagem: "A senha é obrigatória",
+    });
+  }
+
+  conta.usuario.nome = nome;
+  conta.usuario.cpf = cpf;
+  conta.usuario.data_nascimento = data_nascimento;
+  conta.usuario.telefone = telefone;
+  conta.usuario.email = email;
+  conta.usuario.senha = senha;
+
+  return res.status(200).json({ mensagem: "requisição bem sucedida" });
+};
+
+const deletarConta = (req, res) => {
+  const { numero } = req.params;
+  const conta = contas.find((conta) => {
+    return conta.numero === Number(numero);
+  });
+
+  if (!conta) {
+    return res.status(404).json({ mensagem: "conta não encontrada" });
+  }
+  if (conta.saldo !== 0) {
+    return res
+      .status(400)
+      .json({ mensagem: "A conta só pode ser removida se o saldo for zero!" });
+  }
+  contas = contas.filter((conta) => {
+    return conta.numero !== Number(numero);
+  });
+
+  return res.status(204).send();
+};
 
 const depositarConta = (req, res) => {};
 
@@ -75,7 +145,7 @@ const sacarConta = (req, res) => {};
 
 const transferirConta = (req, res) => {};
 
-const consultarConta = (req, res) => {};
+const consultarSaldoConta = (req, res) => {};
 
 const extratoConta = (req, res) => {};
 
@@ -88,5 +158,5 @@ module.exports = {
   extratoConta,
   sacarConta,
   transferirConta,
-  consultarConta,
+  consultarSaldoConta,
 };
--- src/roteador.js
+++ src/roteador.js
@@ -3,12 +3,25 @@ const {
   criarConta,
   listarContas,
   atualizarConta,
+  deletarConta,
+  depositarConta,
+  sacarConta,
+  transferirConta,
+  consultarSaldoConta,
+  extratoConta,
 } = require("./controladores/dados");
 const validarSenhaBanco = require("./intermediarios/validar-senha-banco");
+const validarSenhaConta = require("./intermediarios/validar-senha-conta");
 
 const rotas = express();
 
 rotas.post("/contas", criarConta);
-rotas.get("/contas", validarSenhaBanco, listarContas);
+rotas.post("/transacoes/depositar", depositarConta);
+rotas.post("/transacoes/sacar", sacarConta);
+rotas.post("/transacoes/transferir", transferirConta);
 rotas.put("/contas/:numeroConta/usuario", atualizarConta);
+rotas.delete("/contas/:numeroConta", deletarConta);
+rotas.get("/contas", validarSenhaBanco, listarContas);
+rotas.get("/contas/saldo", validarSenhaConta, consultarSaldoConta);
+rotas.get("/contas/extrato", validarSenhaConta, extratoConta);
 module.exports = rotas;
